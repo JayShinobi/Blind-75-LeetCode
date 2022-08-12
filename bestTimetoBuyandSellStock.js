5b0b8cec60f51cd6038d5096c5db48b1043cf1c8
Jayme Jackson <101202082+JayShinobi@users.noreply.github.com>
2022-08-11 23:19:22 -0700

    Add files via upload
    best Time to Buy and Sell Stock

---
+++ bestTimetoBuyandSellStock.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+    let maxProfit = 0
+    let min = prices[0]
+    for(let i = 1; i < prices.length; i++){
+        min = Math.min(min, prices[i])
+        let profit = prices[i] - min 
+        maxProfit = Math.max(maxProfit, profit)
+    }
+    return maxProfit 
+
+};

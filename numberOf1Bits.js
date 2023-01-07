bce5db4da2faa5ee320564c945615c899f934e62
Jayme Jackson <101202082+JayShinobi@users.noreply.github.com>
2023-01-06 23:20:53 -0800

    Update numberOf1Bits.js

--- numberOf1Bits.js
+++ numberOf1Bits.js
@@ -1,8 +1,7 @@
 var hammingWeight = function(n) {
     let regex = /1/g
     let string = n.toString(2)
+    //use spread iterator to out into array to interate over each match 
       let match = [...string.matchAll(regex)]
       return match.length
-  
-  
   };
